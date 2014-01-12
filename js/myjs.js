1f8a51c2be99ba44630883067d50d17748d4ca94
alsosun <cory.antonini@gmail.com>
2014-01-12 13:39:11 -0600

    poptut

--- js/myjs.js
+++ js/myjs.js
@@ -926,24 +926,24 @@ $(document).ready(function () {
     //    }
     //});
     ////vid 2
-    ////$("#popupVideo-2 iframe")
-    ////.attr("width", 0)
-    ////.attr("height", 0);
+    $("#popupVideo-2 iframe")
+    .attr("width", 0)
+    .attr("height", 0);
 
-    ////$("#popupVideo-2").on({
-    ////    popupbeforeposition: function () {
-    ////        var size = scale(497, 298, 15, 1),
-    ////            w = size.width,
-    ////            h = size.height,
-    ////            markup = " <iframe src='http://www.youtube.com/embed/IUtff1owJZA?rel=0' width='" + w + "' height='" + h + "'  seamless></iframe>";
+    $("#popupVideo-2").on({
+        popupbeforeposition: function () {
+            var size = scale(497, 298, 15, 1),
+                w = size.width,
+                h = size.height,
+                markup = " <iframe src='http://www.youtube.com/embed/1cp2YgrU3ws?rel=0' width='" + w + "' height='" + h + "'  seamless></iframe>";
 
-    ////        $("#popupVideo-2").html(markup);
+            $("#popupVideo-2").html(markup);
 
-    ////    },
-    ////    popupafterclose: function () {
-    ////        $("#popupVideo-2").html("<span></span>");
-    ////    }
-    ////});
+        },
+        popupafterclose: function () {
+            $("#popupVideo-2").html("<span></span>");
+        }
+    });
     ////vid 3
     //$("#popupVideo-3 iframe")
     //    .attr("width", 0)
