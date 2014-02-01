b6064b9d5e98c966b6e78652d61e80471755b16f
alsosun <cory.antonini@gmail.com>
2014-02-01 10:32:38 -0600

    enter key

--- js/myjs.js
+++ js/myjs.js
@@ -1073,4 +1073,9 @@ $(document).ready(function () {
             showLoadMsg: true
         });
     };
+    $("#picpop").keydown(function (e) {
+        if (e.keyCode == 13) {
+            $("##picpopdone").click();
+        }
+    });
 });
